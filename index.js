c2de872dcd331f602f286625c2049168e0038e0b
khoatran162000 <khoatranchemistry@gmail.com>
2022-11-28 18:29:54 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/userRoutes");
+const postRoute = require("./routes/postRoutes");
+const categoryRoute = require("./routes/categoriesRoutes");
+const multer = require("multer");
+const path = require("path");
+
+dotenv.config();
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name); // When testing on Postman change req.body.name to file name
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+app.listen("5000", () => {
+  console.log("Backend Running - Listening on Port 5000");
+});
---
+++ routes/categoriesRoutes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const Category = require("../models/Category");
+
+// Add New Category
+router.post("/", async (req, res) => {
+  const newCategory = new Category(req.body);
+  try {
+    const savedCategory = await newCategory.save();
+    res.status(200).json(savedCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get All Categories
+router.get("/", async (req, res) => {
+  try {
+    const cats = await Category.find();
+    res.status(200).json(cats);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
